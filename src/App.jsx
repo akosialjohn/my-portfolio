0670e8ff5e1e25e6be46f64742ad03c0d378af06
Aljohn Bareo <you@example.comaljohn.bareo.pixel8@gmail.com>
2025-09-11 13:59:51 +0800

    page responiveness complete

--- src/App.jsx
+++ src/App.jsx
@@ -5,85 +5,129 @@ import { MdOutlineDeveloperMode } from "react-icons/md";
 import { MdOutlineDashboardCustomize } from "react-icons/md";
 import { FaFacebook, FaInstagramSquare, FaLinkedinIn, FaYoutubeSquare, FaSpotify } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
+import { GiHamburgerMenu } from "react-icons/gi";
 
+function Navbar({activeTab, setActiveTab}){
+  const [menuOpen, setMenuOpen] = useState (false);
+  const tabs = [
+    {name: "Home", label:"Home"},
+    {name: "About", label:"About Me"},
+    {name: "Services", label:"Services Offered"},
+    {name: "Projects", label:"Projects"},
+  ];
+  
+  return(
+    <nav className="fixed top-0 left-0 w-full bg-blue-950">
+      <div className="flex justify-between items-center px-4 md:px-10 py-4">
+        {/* Hamburger Menu for Mobile View */}
+        <div className="md:hidden">
+          <button onClick={() => setMenuOpen(!menuOpen)}>
+            <GiHamburgerMenu className="text-white w-8 h-8" />
+          </button>
+        </div>
+
+        {/* Desktop Menu */}
+        <ul className="hidden md:flex justify-center space-x-8 text-lg font-medium w-full">
+          {tabs.map((tab) => (
+            <li key={tab.name}>
+              <button className={activeTab === tab.name ? "text-blue-500 font-extrabold underline underline-offset-4" : "text-white cursor-pointer hover:text-orange-500"}
+              onClick={() => setActiveTab(tab.name)}>{tab.label}</button>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+        {/* Mobile Menu */}
+        {menuOpen && (
+          <ul className="flex flex-col md:hidden space-y-2 px-4 pb-4">
+            {tabs.map((tab) => (
+              <li key={tab.name}>
+                <button className={activeTab === tab.name ? "text-blue-500 font-extrabold underline underline-offset-4" : "text-white cursor-pointer hover:text-orange-500"}
+                  onClick={() => {setActiveTab(tab.name); setMenuOpen(false);}}>{tab.label}</button>
+              </li>
+            ))}
+          </ul>
+        )}
+    </nav>
+  )
+}
 
 function App(){
   const [activeTab, setActiveTab] = useState("Home");
   const Tabs = {
     Home: (
-      <section className="flex justify-between items-start bg-orange-500 ">
-      <div className="flex items-start gap-4 p-10">
-          <h1 className="text-black text-[20rem] font-extrabold">&#123;</h1>
-            <div className="flex flex-col">
-            <h1 className="text-black text-8xl font-extrabold leading-tight pt-30">
+      <section className="flex flex-col md:flex-row min-h-screen justify-between items-start bg-orange-500 ">
+      <div className="flex md:items-start p-10 min-h-screen">
+          <h1 className="text-black text-[10rem] md:text-[20rem] font-extrabold">&#123;</h1>
+            <div className="flex flex-col md:items-start pt-16">
+            <h1 className="text-black text-4xl md:text-8xl font-extrabold leading-tight md:pt-14">
               Aljohn <br /> Bareo
             </h1>
-            <p className="text-black text-4xl font-extrabold leading-tight pt-6">WEB DEVELOPMENT SERVICES</p>
+            <p className="text-black text-xl pt-2 md:text-4xl font-extrabold leading-tight">WEB DEVELOPMENT SERVICES</p>
           </div>
       </div>
 
-      <div className="flex flex-col justify-end">
+      <div className="md:justify-end">
         <img
           src={ProfilePicture}
           alt="My Profile"
-          className="w-150 h-180 pt-6"
+          className="hidden md:block w-120 h-160 md:pt-6 md:w-120 md:h-180"
         />
-        <h1 className="text-white text-center text-2xl font-sans bg-black px-4 py-25">
+        <h1 className="text-white text-center text-2xl font-sans bg-black px-18 py-25  -mt-60 md:mt-0 md:px-4 md:py-25">
           Your trusted web developer <br /> for your startup business.
         </h1>
       </div>
     </section>
     ),
     About: (
-      <section className="flex h-screen">
-        <div className="w-3/4 bg-black">
+      <section className="md:flex md:h-screen">
+        <div className="md:w-3/4 md:flex-col bg-black md:flex justify-center pt-14">
          <img
           src={ProfilePicture}
           alt="My Profile"
-          className="w-150 h-180 pt-6"
+          className="md:pt-6 md:w-120 md:h-180 h-90 w-60"
         /></div>
-        <div className="w-1/3 bg-orange-500">
-          <h1 className="text-white justify-end text-6xl pt-30 pl-20 font-bold font-sans">About <br /> Aljohn</h1>
-          <p className="justify-center items-center text-2xl text-white p-20">Hi! I’m Aljohn Bareo, a passionate frontend developer specializing in creating modern, responsive, and user-friendly websites. 
+        <div className="md:w-1/3 bg-orange-500">
+          <h1 className="text-white justify-end text-4xl md:text-6xl pt-10 pl-10 md:pt-30 md:pl-20 font-bold font-sans">About <br /> Aljohn</h1>
+          <p className="justify-center items-center text-xl md:text-2xl text-white p-10">Hi! I’m Aljohn Bareo, a passionate frontend developer specializing in creating modern, responsive, and user-friendly websites. 
             <br /> I enjoy turning ideas into interactive digital experiences using React, Tailwind CSS, and JavaScript. 
             <br /> My goal is to build websites that not only look great but also deliver seamless functionality for users.</p>
         </div>
       </section>
     ),
     Services: (
-      <section className="max-h-1/2 w-full">
-        <div className="h-screen bg-orange-50">
-            <div className="flex flex-col p-20">
-              <h1 className="text-6xl m-10 text-orange-500 font-mono font-extrabold text-left">Services <br /> Offered</h1>
+      <section className="w-full bg-orange-50">
+            <div className="flex flex-col md:p-20 pt-20">
+              <h1 className="text-6xl m-10 text-orange-500 font-mono font-extrabold text-center md:text-left ">Services <br /> Offered</h1>
           </div>
-        </div>
-        <div className="absolute w-full h-1/2 bottom-0 flex">
-          <div className="w-1/3 bg-orange-500">
+
+        <div className="w-full flex flex-col md:flex-row pt-20">
+          <div className="w-full md:w-1/3 bg-orange-500">
             <div className="p-6 text-orange-50">
               <h3 className="text-xl font-semibold mb-2">Website Development</h3>
               <p className="text-orange-50">Build responsive, visually appealing websites using HTML, CSS, and JavaScript.</p>
-              <div className="flex justify-center"><CgWebsite className="h-60 text-[15rem]"/></div>
+              <div className="flex justify-center"><CgWebsite className="h-60 md:text-[15rem] text-9xl"/></div>
             </div>
           </div>
-          <div className="w-1/3 bg-black">
+          <div className="w-full md:w-1/3 bg-black">
             <div className="p-6 text-orange-50">
               <h3 className="text-xl font-semibold mb-2">Web Application Development</h3>
               <p className="text-orange-50">Develop interactive web apps with frameworks/libraries like React, Vue, Angular, or Svelte.</p>
-              <div className="flex justify-center"><MdOutlineDeveloperMode className="h-60 text-[15rem]"/></div>
+              <div className="flex justify-center"><MdOutlineDeveloperMode className="h-60 md:text-[15rem] text-9xl"/></div>
               </div>
           </div>
-          <div className="w-1/3 bg-orange-500">
+          <div className="w-full md:w-1/3 bg-orange-500">
             <div className="p-6 text-orange-50">
               <h3 className="text-xl font-semibold mb-2">UI/UX Implementation</h3>
               <p className="text-orange-50">Transform UI/UX designs into functional web interfaces.</p>
-              <div className="flex justify-center"><MdOutlineDashboardCustomize className="h-60 text-[15rem]"/></div>
+              <div className="flex justify-center"><MdOutlineDashboardCustomize className="h-60 md:text-[15rem] text-9xl"/></div>
             </div>
           </div>
          </div>
       </section>
     ),
     Projects: (
-      <section className="h-screen bg-gray-50 flex flex-col items-center p-20">
+      <section className="bg-orange-50 flex flex-col items-center p-20">
         <h2 className="text-4xl font-bold mb-10">Projects</h2>
         <div className="grid md:grid-cols-3 gap-6 w-full max-w-5xl">
           <div className="p-6 bg-white shadow rounded-xl hover:shadow-lg transition">
@@ -100,50 +144,15 @@ function App(){
           </div>
         </div>
       </section>
-    ),
+    )
   };
   return (
     <div>
       {/* Navbar */}
-      <nav className="fixed top-0 left-0 w-full bg-blue-900 shadow z-50">
-        <ul className="flex justify-center space-x-8 py-4 text-lg font-medium">
-          <li>
-            <button
-              className={activeTab === "Home" ? "text-black font-extrabold" : "text-white cursor-pointer hover:text-orange-500"}
-              onClick={() => setActiveTab("Home")}
-            >
-              Home
-            </button>
-          </li>
-          <li>
-            <button
-              className={activeTab === "About" ? "text-black font-extrabold" : "text-white cursor-pointer hover:text-orange-500"}
-              onClick={() => setActiveTab("About")}
-            >
-              About Me
-            </button>
-          </li>
-          <li>
-            <button
-              className={activeTab === "Services" ? "text-black font-extrabold" : "text-white cursor-pointer hover:text-orange-500"}
-              onClick={() => setActiveTab("Services")}
-            >
-              Services Offered
-            </button>
-          </li>
-          <li>
-            <button
-              className={activeTab === "Projects" ? "text-black font-extrabold" : "text-white cursor-pointer hover:text-orange-500"}
-              onClick={() => setActiveTab("Projects")}
-            >
-              Projects
-            </button>
-          </li>
-        </ul>
-      </nav>
+      <Navbar activeTab = {activeTab} setActiveTab = {setActiveTab} />
 
       {/* Sections */}
-      <main className="pt-5">{Tabs[activeTab]}</main>
+      <main className="pt-2">{Tabs[activeTab]}</main>
       <footer className="bg-orange-50">
         <h2 className="text-orange-500 font-semibold text-center">KEEP IN TOUCH</h2>
         <div className="flex flex-row space-x-4 items-center justify-center">
